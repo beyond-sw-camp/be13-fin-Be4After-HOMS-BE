45f639db117eddda756e966df333aee2f48a1fd4
g00dbyul <skypower4203@gmail.com>
2025-05-02 14:51:39 +0900

    feat : definition user related entity
    유저 관련 엔티티 생성

---
+++ homs/src/main/java/com/beyond/homs/user/entity/User.java
@@ -0,0 +1,63 @@
+package com.beyond.homs.user.entity;
+
+import com.beyond.homs.user.data.UserRole;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "user_name",nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "managerName",nullable = false)
+    private String managerName;
+
+    @Column(name = "managerEmail",nullable = false)
+    private String managerEmail;
+
+    @Column(name = "managerPhone",nullable = false)
+    private String managerPhone;
+
+    @Column(name = "role",nullable = false)
+    private UserRole role;
+
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public User(String userName, String managerName, String managerEmail, String managerPhone, UserRole role) {
+        this.userName = userName;
+        this.managerName = managerName;
+        this.managerEmail = managerEmail;
+        this.managerPhone = managerPhone;
+        this.role = role;
+    }
+}
---
+++ homs/src/main/java/com/beyond/homs/user/entity/UserLogin.java
@@ -0,0 +1,33 @@
+package com.beyond.homs.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_login")
+@Getter
+@NoArgsConstructor
+public class UserLogin {
+    @Id
+    @Column(name = "user_id")
+    private Long userId;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "is_locked_out", nullable = false)
+    private boolean isLockedOut;
+}
