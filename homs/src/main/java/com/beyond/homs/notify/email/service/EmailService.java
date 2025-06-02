858d5d9cc32ceeafc6f8b6803790bc7a94e76d1e
MSP_31 <jhss9747@gmail.com>
2025-06-02 12:01:13 +0900

    feat: "이메일 세분화 및 템플릿 수정"

--- homs/src/main/java/com/beyond/homs/notify/email/service/EmailService.java
+++ homs/src/main/java/com/beyond/homs/notify/email/service/EmailService.java
@@ -1,9 +1,8 @@
 package com.beyond.homs.notify.email.service;
 
+import com.beyond.homs.notify.email.data.EmailTypeEnum;
 import com.beyond.homs.notify.email.entity.EmailMessage;
 
-import java.util.Map;
-
 public interface EmailService {
-    String sendMail(EmailMessage emailMessage, String type);
+    String sendMail(EmailMessage emailMessage, EmailTypeEnum type);
 }
